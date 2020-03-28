58ce7c7f91eb18c595213ce16ad759c3d30db4e3
dallas_city <dallas@unnotechmanila.com>
2020-03-28 20:41:04 +0800

    Redirect to 404 Pages on non-existing pages

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="error-page">
+    <!-- <h1 class="text-center">
+      <p>{{ message }}</p>
+      <nuxt-link to="/">Return to homepage</nuxt-link>
+    </h1> -->
+    <b-alert show variant="danger">
+      <h4 class="alert-heading">{{ message }}</h4>
+      <nuxt-link to="/">Return to homepage</nuxt-link>
+    </b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtError",
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500;
+    },
+    message() {
+      return this.error.message;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#error-page {
+  padding: 15px;
+}
+</style>
