76f66fa257fe4dc556e67188038aa9dc630c5f65
dallas_city <dallas@unnotechmanila.com>
2020-05-10 00:52:43 +0800

    Temporary minimal testing in Login

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -1,26 +1,29 @@
-// import Vue from "vue";
 import { mount, createLocalVue } from "@vue/test-utils";
 import { ValidationObserver, ValidationProvider } from "vee-validate";
 import { BootstrapVue } from "bootstrap-vue";
-// import Notifications from "vue-notification";
+import Notifications from "vue-notification";
+// import flushPromises from "flush-promises";
 import Login from "@/pages/login.vue";
+// import { delay } from "@/test/setup.js";
 
 const localVue = createLocalVue();
 localVue.component("ValidationObserver", ValidationObserver);
 localVue.component("ValidationProvider", ValidationProvider);
 localVue.use(BootstrapVue);
+localVue.use(Notifications);
 
 jest.useFakeTimers();
 
 describe("Login", () => {
-  test("if username is not entered", () => {
-    const wrapper = mount(Login, { localVue });
+  test("if username is not entered", async () => {
+    const wrapper = mount(Login, { localVue, sync: false });
     const username = wrapper.find("#username");
-    const password = wrapper.find("#password");
-    const login = wrapper.find("#login");
-    jest.advanceTimersByTime(50);
-    username.element.value = "test";
-    password.element.value = "test";
-    expect(login.attributes("disabled")).toBe(true);
+    // const usernameError = wrapper.find("#username-error");
+    // const login = wrapper.find("#login");
+    username.setValue(" ");
+    // await flushPromises();
+    // expect(usernameError.text()).toBeTruthy();
+    // jest.advanceTimersByTime(50);
+    // expect(login.attributes("disabled")).toBe(true);
   });
 });
